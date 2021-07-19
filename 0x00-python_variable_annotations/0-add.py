baaebebd9c64ec004ec35662cf6b7bce45923e81
karim <karim-bahri-18@outlook.fr>
2021-07-19 05:44:10 +0100

    task-0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""add function"""
+
+
+def add(a: float, b: float) -> float:
+    """annotated function
+        return the sum of a and b float typed
+    """
+    return a + b
