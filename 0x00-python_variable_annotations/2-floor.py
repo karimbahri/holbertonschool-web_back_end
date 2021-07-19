4b2cc5a7f9df37345f2f9ce58300c0ab4d429505
karim <karim-bahri-18@outlook.fr>
2021-07-19 05:49:07 +0100

    tast-2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""floor function"""
+from math import floor as flr
+
+
+def floor(n: float) -> int:
+    """floor : return floor of float
+        it takes a float as argument
+        return integer
+    """
+    return flr(n)
