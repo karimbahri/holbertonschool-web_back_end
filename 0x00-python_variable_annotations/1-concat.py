120f5d816084845940759fdec83dbc7e62abeed6
karim <karim-bahri-18@outlook.fr>
2021-07-19 05:48:56 +0100

    tast-1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""concat function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat: a function that concat two strings
+        it takes two arguments (string)
+        return the result of concatenation of str1 and str2
+    """
+    return str1 + str2
