3bc3ff425a61cef106fd74360de9a7da5019a0e4
karim <karim-bahri-18@outlook.fr>
2021-07-19 05:49:23 +0100

    tast-5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""5-sum_list.py"""
+import typing
+
+
+def sum_list(input_list: typing.List[float]) -> float:
+    """sum_list :
+    return the sum of of items of list
+    """
+    return sum(input_list)
