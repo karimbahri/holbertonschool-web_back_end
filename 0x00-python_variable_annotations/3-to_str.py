e2ca5841c535767d9272b3df7408e4dade933ddb
karim <karim-bahri-18@outlook.fr>
2021-07-19 05:49:12 +0100

    tast-3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""to_str"""
+
+
+def to_str(n: float) -> str:
+    """to_str:
+        return string repr of float
+        it takes float as argument
+        return string
+    """
+    return str(n)
