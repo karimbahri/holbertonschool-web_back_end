fe2d9ce29abf3147fb13af963f947fcf7b15300a
karim <karim-bahri-18@outlook.fr>
2021-07-21 20:05:06 +0100

    fix gather instuction

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """2-measure_runtime"""
-import time
+from time import time
 import asyncio
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
@@ -10,7 +10,8 @@ async def measure_runtime() -> float:
     measure_runtime: a coroutine that execute async_comprehension
     4 times and mesure the runtime
     """
-    beg = time.time()
-    await asyncio.gather(*[async_comprehension() for i in range(4)])
-    runtime = time.time() - beg
+    beg = time()
+    instructions = [async_comprehension() for i in range(4)]
+    await asyncio.gather(*instructions)
+    runtime = time() - beg
     return runtime
