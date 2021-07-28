fb35a7335ba5bdaf39ffdc458d55d50513fe6510
karim <karim-bahri-18@outlook.fr>
2021-07-28 17:10:49 +0100

    get hyper

--- 0x04-pagination/2-hypermedia_pagination.py
+++ 0x04-pagination/2-hypermedia_pagination.py
@@ -60,7 +60,7 @@ class Server:
             'page_size': len(self.get_page(page, page_size)),
             'page': page,
             'data': self.get_page(page, page_size),
-            'next_page': page + 1 if page <= all else None,
+            'next_page': page + 1 if page < all else None,
             'prev_page': page - 1 if page > 1 else None,
             'total_pages': all
         }
