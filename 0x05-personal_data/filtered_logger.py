0abeaa904651eb61200573db49968eefbeba18ca
karim <karim-bahri-18@outlook.fr>
2021-08-01 10:03:18 +0100

    regex

---
+++ 0x05-personal_data/filtered_logger.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""filtered_logger.py"""
+import typing
+import re
+import logging
+
+
+def filter_datum(fields: typing.List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    """filter_datum:
+        obfuscate log message
+    """
+    for element in fields:
+        message = re.sub(r"{}=.+?{}".format(element, separator),
+                         "{}={}{}".format(element,
+                                          redaction, separator), message)
+    return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        NotImplementedError
