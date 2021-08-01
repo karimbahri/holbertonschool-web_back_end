b89f61045a1d300a2a7b8082893c03ea74901b67
karim <karim-bahri-18@outlook.fr>
2021-08-01 16:30:13 +0100

    pcstyle

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -29,6 +29,7 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        """constructor function of Redacting Formatter class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
