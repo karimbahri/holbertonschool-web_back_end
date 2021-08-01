2b86f7c6725a625aaeef9efdb82fd77d6a785cec
karim <karim-bahri-18@outlook.fr>
2021-08-01 19:16:14 +0100

    pcstyle

--- 0x05-personal_data/filtered_logger.py
+++ 0x05-personal_data/filtered_logger.py
@@ -28,7 +28,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
+    def __init__(self, fields: typing.List[str]):
         """constructor function of Redacting Formatter class"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
