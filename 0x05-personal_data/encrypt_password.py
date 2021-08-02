fc46be90d61fbdedc36570ff65c4b30cac7efa65
karim <karim-bahri-18@outlook.fr>
2021-08-02 11:41:37 +0100

    check password

--- 0x05-personal_data/encrypt_password.py
+++ 0x05-personal_data/encrypt_password.py
@@ -9,3 +9,13 @@ def hash_password(password: str) -> bytes:
         (string) and return a hashed password
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """is_valid
+        check the matching password with the hashed version
+        it expects hashed_password(bytes) and password str
+        as arguments
+        and return a boolean (true/false)
+    """
+    return bcrypt.checkpw(password, hashed_password)
