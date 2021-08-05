fecb01f081d11cff6d6e840b81e86f4edf30deee
karim <karim-bahri-18@outlook.fr>
2021-08-05 17:54:28 +0100

    basic auth 64auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """basic_auth.py"""
 from api.v1.auth.auth import Auth
+from base64 import b64decode
 
 
 class BasicAuth(Auth):
@@ -20,3 +21,19 @@ class BasicAuth(Auth):
             return None
         authorization64_header = authorization_header[6:]
         return authorization64_header
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:
+                                           str) -> str:
+        """decode_base64_authorization_header:
+            public method
+        """
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            return b64decode(base64_authorization_header)\
+                            .decode('utf-8')
+        except Exception:
+            return None
