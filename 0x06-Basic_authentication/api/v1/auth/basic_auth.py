b54f1e1eb59064c89f324abf1184c85ff1211433
karim <karim-bahri-18@outlook.fr>
2021-08-05 17:41:44 +0100

    basic auth 64auth

--- 0x06-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x06-Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,4 +7,16 @@ class BasicAuth(Auth):
     """BasicAuth:
         class that inherit from auth
     """
-    pass
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """extract authorization
+            public method: return 64base authorization
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith('Basic'):
+            return None
+        authorization64_header = authorization_header[6:]
+        return authorization64_header
