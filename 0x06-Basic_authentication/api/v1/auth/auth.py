bb9e8a49ef63b426de46f4a92b3788a96653cfcd
karim <karim-bahri-18@outlook.fr>
2021-08-05 14:49:42 +0100

    authCheck

--- 0x06-Basic_authentication/api/v1/auth/auth.py
+++ 0x06-Basic_authentication/api/v1/auth/auth.py
@@ -16,14 +16,13 @@ class Auth:
             check for authentication
             return boolean
         """
-        if not path or not excluded_paths:
+        if path is None or excluded_paths is None or not len(excluded_paths):
             return True
-        if not len(excluded_paths):
-            return True
-        if path in excluded_paths or path[-1] == '/':
-            return False
         if path[-1] != '/':
             path += '/'
+        if path in excluded_paths:
+            return False
+        else:
             return True
 
     def authorization_header(self, request=None) -> str:
