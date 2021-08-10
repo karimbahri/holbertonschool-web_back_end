9e9c0d4ec726c078b4ddaaa59f2f70fd07696faf
karim <karim-bahri-18@outlook.fr>
2021-08-10 15:35:08 +0100

    fix auth type

--- 0x06-Basic_authentication/api/v1/app.py
+++ 0x06-Basic_authentication/api/v1/app.py
@@ -14,10 +14,10 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 auth = None
 auth_type = getenv('AUTH_TYPE')
-if auth == 'auth':
+if auth_type == 'auth':
     from api.v1.auth.auth import Auth
     auth = Auth()
-elif auth == 'basic_auth':
+elif auth_type == 'basic_auth':
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth
 
