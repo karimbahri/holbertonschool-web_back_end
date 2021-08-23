9777dc804ce9082d1567fcdf5dc9dc580612a7b9
karim <karim-bahri-18@outlook.fr>
2021-08-24 00:02:06 +0100

    mock

--- 0x09-Unittests_and_integration_tests/test_utils.py
+++ 0x09-Unittests_and_integration_tests/test_utils.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """test_utils"""
 import unittest
+from unittest import mock
 from parameterized import parameterized
 from utils import access_nested_map
+from utils import memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
@@ -28,3 +30,28 @@ class TestAccessNestedMap(unittest.TestCase):
         raise exception
         test method:"""
         self.assertRaises(KeyError, access_nested_map, t_map, t_path)
+
+
+"""class TestGetJson(unittest.TestCase):
+    TestGetJson class
+        json requests tests
+    """
+
+
+class TestMemoize(unittest.TestCase):
+    """TestMemoize class"""
+    def test_memoize(self):
+        """test_memoize"""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+        with mock.patch.object(TestClass, 'a_method') as mk:
+            t_class = TestClass()
+            self.assertEqual(t_class.a_property, mk.return_value)
+            self.assertEqual(t_class.a_property, mk.return_value)
+            mk.assert_called_once()
