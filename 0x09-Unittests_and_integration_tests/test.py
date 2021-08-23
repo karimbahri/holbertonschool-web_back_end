991a94237cfd94f6eab52aac7a07849cb3371f88
karim <karim-bahri-18@outlook.fr>
2021-08-23 12:39:51 +0100

    getResults file

---
+++ 0x09-Unittests_and_integration_tests/test.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""test"""
+from utils import access_nested_map
+
+if __name__ == "__main__":
+    print(access_nested_map({"a": 1}, ("a",)))
+    print(access_nested_map({"a": {"b": 2}}, ("a",)))
+    print(access_nested_map({"a": {"b": 2}}, ("a", "b")))
