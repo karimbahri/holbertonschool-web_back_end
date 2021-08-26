8dde16854fe540e3f164d0b1689ddac7f866eb43
karim <karim-bahri-18@outlook.fr>
2021-08-26 15:18:43 +0100

    renderhtml

---
+++ 0x0A-i18n/0-app.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""app"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def render_html():
+    """render html:
+        function that return the rendering html file
+    """
+    return render_template('0-index.html')
