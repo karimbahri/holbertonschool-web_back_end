92def95bfb28af39d7b4f681d2ec0e8721264a85
karim <karim-bahri-18@outlook.fr>
2021-08-26 15:57:24 +0100

    renderhtml

--- 0x0A-i18n/0-app.py
+++ 0x0A-i18n/0-app.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
-"""app"""
+"""app.py module
+    rendering html file
+    using flask
+
+"""
 from flask import Flask
 from flask import render_template
 
@@ -9,7 +13,9 @@ app = Flask(__name__)
 @app.route('/')
 def render_html():
     """render html:
-        function that return the rendering html file
+        function that return
+        the rendering the given
+        html filels
     """
     return render_template('0-index.html')
 
