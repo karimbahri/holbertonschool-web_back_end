f41597424052f11c2835f7ceeaf11eed44ec6aab
karim <karim-bahri-18@outlook.fr>
2021-08-31 14:35:05 +0100

    get int

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 """exercice module"""
 from typing import Union
+from typing import Optional
+from typing import Callable
+from typing import Any
 
 from redis import Redis
 from uuid import uuid4
@@ -32,3 +35,30 @@ class Cache:
         _id = str(uuid4())
         self._redis.set(_id, data)
         return _id
+
+    def get(self, key: str, fn: Optional[Callable] = None) -> Any:
+        """public method:
+            takes key (string) as argument
+            and an optional callable function
+            to convert data format
+        """
+        d = self._redis.get(key)
+
+        if fn is None:
+            return d
+        else:
+            return fn(d)
+
+    def get_str(self, id):
+        """get_str: public method
+            change data format to string
+        """
+        converted = self.get(id)
+        return converted
+
+    def get_int(self, id):
+        """get_int: public method
+            change data format to integer
+        """
+        converted = int(self.get(id))
+        return converted
