b36f4d6fade9e3529f28c6e862a1adb9ce2a997f
karim <karim-bahri-18@outlook.fr>
2021-08-31 14:35:26 +0100

    main

--- 0x0B_redis_basic/main.py
+++ 0x0B_redis_basic/main.py
@@ -8,9 +8,13 @@ Cache = __import__('exercise').Cache
 
 cache = Cache()
 
-data = b"hello"
-key = cache.store(data)
-print(key)
+TEST_CASES = {
+    b"foo": None,
+    123: int,
+    "bar": lambda d: d.decode("utf-8")
+}
 
-local_redis = redis.Redis()
-print(local_redis.get(key))
+for value, fn in TEST_CASES.items():
+    key = cache.store(value)
+    assert cache.get(key, fn=fn) == value
+    
