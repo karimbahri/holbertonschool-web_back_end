5204059aff29a99ac6b494129b3b3953cbeab165
karim <karim-bahri-18@outlook.fr>
2021-09-02 15:34:06 +0100

    create table with countries

---
+++ 0x0C-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,6 @@
+-- create table 'users' with attributes id, email, name, country
+CREATE TABLE IF NOT EXISTS users (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, 
+email VARCHAR(255) UNIQUE NOT NULL,
+name VARCHAR(255),
+country ENUM('US', 'CO', 'TN') NOT NULL
+)
