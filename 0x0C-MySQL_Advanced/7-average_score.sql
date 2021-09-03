3e6835a31abd9400d4d1796791f556c7310d0762
karim <karim-bahri-18@outlook.fr>
2021-09-03 21:14:50 +0100

    average

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,3 +1,4 @@
 -- procedure creation to calculate average score
+DELIMITER _|
 CREATE PROCEDURE ComputeAverageScoreForUser(user_id INT)
-BEGIN UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id) WHERE user_id = id; END;
+BEGIN UPDATE users SET average_score = (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id) WHERE user_id = id; END _|
