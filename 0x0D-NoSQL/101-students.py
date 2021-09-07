6ca914c572540db5ef2a822aa2ad0b48e8f9687e
karim <karim-bahri-18@outlook.fr>
2021-09-07 20:03:01 +0100

    aggregate

---
+++ 0x0D-NoSQL/101-students.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""101-students"""
+
+
+def top_students(mongo_collection):
+    """top_students:
+        using mongodb aggregation feature
+        to sort projects by average score
+    """
+    stages = [
+        {
+            '$group': {
+                '_id': 'null',
+                'name': '$name',
+                'averageScore': {'$avg': :'$topics.score'}
+            }
+        },
+        {
+            {'$sort': {'averageScore': -1}}
+        },
+        {
+            '$project': {
+                '_id': 0
+            }
+        }
+    ]
+    mongo_collection.aggregate(stages)
