d3036aed2d0cbdb23cd506b7db0585e7673d39f0
karim <karim-bahri-18@outlook.fr>
2021-09-07 17:40:58 +0100

    find topics

---
+++ 0x0D-NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+""" 11-schools_by_topics """
+
+
+def schools_by_topic(mongo_collection, topic):
+    """schools_by_topic:
+        a function that return documents that matches
+        with topic
+    """
+    return list(mongo_collection.find({'topic': topic}))
