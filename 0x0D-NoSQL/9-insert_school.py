638c23090188611c769479a67803e1af08d565a9
karim <karim-bahri-18@outlook.fr>
2021-09-07 17:00:41 +0100

    insertion pymongo

--- 0x0D-NoSQL/9-insert_school.py
+++ 0x0D-NoSQL/9-insert_school.py
@@ -6,4 +6,4 @@ def insert_school(mongo_collection, **kwargs):
     """inset_school
         insert document into given collection
     """
-    return mongo_collection.insert_one(kwargs).insert_id
+    return mongo_collection.insert_one(kwargs).inserted_id
