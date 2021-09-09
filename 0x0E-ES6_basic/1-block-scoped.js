ba201b4638e0e0416eeed52fa43c890404258ff0
karim <karim-bahri-18@outlook.fr>
2021-09-09 14:44:57 +0100

    disable lint

--- 0x0E-ES6_basic/1-block-scoped.js
+++ 0x0E-ES6_basic/1-block-scoped.js
@@ -1,7 +1,7 @@
 export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
-
+  /* eslint-disable */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
