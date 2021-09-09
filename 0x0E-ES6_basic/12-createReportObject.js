db8838de1f840b75e4df5b278ab4bcd92df92111
karim <karim-bahri-18@outlook.fr>
2021-09-09 17:34:01 +0100

    createReportObject

---
+++ 0x0E-ES6_basic/12-createReportObject.js
@@ -0,0 +1,15 @@
+/* eslint-disable */
+function createEmployeesObject(departmentName, employees) {
+  const objs = [...employees];
+  return {
+    [`${departmentName}`]: objs,
+  };
+}
+
+export default function createReportObject(employeesList) {
+    const size = Object.keys(employeesList).length;
+  return {
+      allEmployees: { ...employeesList },
+      getNumberOfDepartments: () => size,
+  };
+};
