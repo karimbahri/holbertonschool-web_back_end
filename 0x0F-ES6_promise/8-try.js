699f863c0bc655a877f4ca9b0834af1af1ad2c99
karim <karim-bahri-18@outlook.fr>
2021-09-14 16:52:38 +0100

    try; git push

--- 0x0F-ES6_promise/8-try.js
+++ 0x0F-ES6_promise/8-try.js
@@ -1,10 +1,6 @@
+/* eslint-disable */
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    }
-    return numerator / denominator;
-  } catch (err) {
-    console.log(err);
-  }
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  } else return numerator / denominator;
 }
