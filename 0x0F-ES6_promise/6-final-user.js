5563953c1b93a85e256197ccd00ca26b568f595e
karim <karim-bahri-18@outlook.fr>
2021-09-14 16:03:45 +0100

    final user

--- 0x0F-ES6_promise/6-final-user.js
+++ 0x0F-ES6_promise/6-final-user.js
@@ -3,23 +3,16 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return new Promise(() => {
-    let signupPromise = undefined;
-    let uploadPromise = undefined;
-
-    signUpUser(firstName, lastName).then((data) => {
-      signupPromise = data;
-    });
-    uploadPhoto(fileName).catch((err) => {
-      uploadPromise = err.message;
-    });
+  return new Promise(async () => {
     const resolved = {
       status: 'fulfilled',
-      value: signupPromise,
+      value: await signUpUser(firstName, lastName),
     };
     const rejected = {
       status: 'rejected',
-      value: uploadPromise,
+      value: await uploadPhoto(fileName).catch((err) => {
+        err.toString();
+      }),
     };
     return [resolved, rejected];
   });
