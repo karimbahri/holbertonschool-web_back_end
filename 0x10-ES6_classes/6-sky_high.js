5904656bbb8069cc157b604e459c63f738dcbb2a
karim <karim-bahri-18@outlook.fr>
2021-09-16 15:34:12 +0100

    inheritance

--- 0x10-ES6_classes/6-sky_high.js
+++ 0x10-ES6_classes/6-sky_high.js
@@ -1,5 +1,5 @@
 /* eslint-disable */
-import Building from '5-building';
+import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
