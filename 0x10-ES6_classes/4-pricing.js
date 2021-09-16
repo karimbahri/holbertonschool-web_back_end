db65b7802b7038f0d24edba209b678f5b3352f6f
karim <karim-bahri-18@outlook.fr>
2021-09-16 14:07:27 +0100

    pricing currency

--- 0x10-ES6_classes/4-pricing.js
+++ 0x10-ES6_classes/4-pricing.js
@@ -1,5 +1,5 @@
-import Currency from './3-currency';
 /* eslint-disable */
+import Currency from './3-currency';
 export default class Pricing {
   constructor(amount, currency) {
     this.amount = amount;
