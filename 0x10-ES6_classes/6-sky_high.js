9114cc695380471d544626ef0441ef8c6b7cd3f8
karim <karim-bahri-18@outlook.fr>
2021-09-16 15:29:58 +0100

    inheritance

---
+++ 0x10-ES6_classes/6-sky_high.js
@@ -0,0 +1,17 @@
+/* eslint-disable */
+import Building from '5-building';
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+  get floors() {
+    return this._floors;
+  }
+  set floors(floors) {
+    this._floors = floors;
+  }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
