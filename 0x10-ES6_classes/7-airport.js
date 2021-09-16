a59947220a9999d1735170f0cb9302f588402095
karim <karim-bahri-18@outlook.fr>
2021-09-16 16:03:30 +0100

    symbol class

---
+++ 0x10-ES6_classes/7-airport.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
+}
