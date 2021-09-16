e10dfd994c90a45863da41cc25024bbc1a8b88c0
karim <karim-bahri-18@outlook.fr>
2021-09-16 12:52:01 +0100

    getter setter

--- 0x10-ES6_classes/2-hbtn_course.js
+++ 0x10-ES6_classes/2-hbtn_course.js
@@ -6,14 +6,15 @@ export default class HolbertonCourse {
     this.students = students;
   }
   set name(name) {
-    if (typeof name !== 'string') throw new Error('Name must be a string');
+    if (typeof name !== 'string') throw new TypeError('Name must be a string');
     this._name = name;
   }
   get name() {
     return this._name;
   }
   set length(length) {
-    if (typeof length !== 'number') throw new Error('Length must be a number');
+    if (typeof length !== 'number')
+      throw new TypeError('Length must be a number');
     this._length = length;
   }
   get length() {
@@ -24,7 +25,7 @@ export default class HolbertonCourse {
       !(students instanceof Array) ||
       students.forEach((el) => typeof el !== 'string')
     ) {
-      throw new Error('Students must be an array of strings');
+      throw new TypeError('Students must be an array of strings');
     }
     this._students = students;
   }
