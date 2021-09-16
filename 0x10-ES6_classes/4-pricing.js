38ac3e9d3022f4150d8b663fd4f6f8f6ab8f4078
karim <karim-bahri-18@outlook.fr>
2021-09-16 14:06:03 +0100

    pricing currency

---
+++ 0x10-ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+import Currency from './3-currency';
+/* eslint-disable */
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+  get amount() {
+    return this._amount;
+  }
+  set amount(amount) {
+    this._amount = amount;
+  }
+  get currency() {
+    return this._currency;
+  }
+  set currency(currency) {
+    this._currency = currency;
+  }
+  displayFullPrice() {
+    return `${this._amount} ${this.currency._name} (${this.currency._code})`;
+  }
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
