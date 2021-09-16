5b923cd42bc0d0747aa769d695382c8825d33271
karim <karim-bahri-18@outlook.fr>
2021-09-16 16:47:27 +0100

    clone car

---
+++ 0x10-ES6_classes/10-car.js
@@ -0,0 +1,10 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+  cloneCar() {
+    return new this.constructor();
+  }
+}
