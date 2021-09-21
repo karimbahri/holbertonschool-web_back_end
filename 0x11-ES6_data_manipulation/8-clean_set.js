88e0fae8de6a26eed561abadf6eb1149ba1d6608
karim <karim-bahri-18@outlook.fr>
2021-09-21 18:35:42 +0100

    clean set

--- 0x11-ES6_data_manipulation/8-clean_set.js
+++ 0x11-ES6_data_manipulation/8-clean_set.js
@@ -3,7 +3,7 @@ export default function cleanSet(set, startString) {
   const data = Array();
   if (startString !== "" && typeof startString === "string") {
     for (let element of set) {
-      if (element instanceof String)
+      if (typeof element === "string")
         if (element.startsWith(startString))
           data.push(element.slice(startString.length));
     }
