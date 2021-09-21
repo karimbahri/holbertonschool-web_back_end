be47c69a89880d3b7f43d70beab73f46a4504b16
karim <karim-bahri-18@outlook.fr>
2021-09-21 20:23:35 +0100

    queryApi

--- 0x11-ES6_data_manipulation/100-weak.js
+++ 0x11-ES6_data_manipulation/100-weak.js
@@ -1,8 +1,8 @@
 /* eslint-disable */
 export const weakMap = new WeakMap();
 export function queryAPI(end_point) {
-  const edp = weakMap.get(end_point);
-  if (edp >= 5) throw new Error("Endpoint load is high");
   if (weakMap.has(end_point) === false) weakMap.set(end_point, 0);
   weakMap.set(end_point, 1 + weakMap.get(end_point));
+  const edp = weakMap.get(end_point);
+  if (edp >= 5) throw new Error("Endpoint load is high");
 }
