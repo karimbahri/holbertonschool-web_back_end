544ac624596887d2b77cb959d77630965b78fb8a
karim <karim-bahri-18@outlook.fr>
2021-09-21 18:45:49 +0100

    map

---
+++ 0x11-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+/* eslint-disable */
+export default function groceriesList() {
+  return new Map([
+    ["Apples", 10],
+    ["Tomatoes", 10],
+    ["Pasta", 1],
+    ["Rice", 1],
+    ["Banana", 5],
+  ]);
+}
