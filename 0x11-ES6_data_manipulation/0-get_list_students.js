1e37b2aff317ba96fe81e23e1a1452b08ed63032
karim <karim-bahri-18@outlook.fr>
2021-09-21 13:57:06 +0100

    array of objects

---
+++ 0x11-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+export default function getListStudents() {
+  return [
+    {
+      firstName: 'Guillaume',
+      id: 1,
+      location: 'San Francisco',
+    },
+    {
+      firstName: 'James',
+      id: 2,
+      location: 'Columbia',
+    },
+    {
+      firstName: 'Serena',
+      id: 5,
+      location: 'San Francisco',
+    },
+  ];
+}
