998a9dd3b52093a0bb088a3a89fce107089a4c52
karim <karim-bahri-18@outlook.fr>
2021-09-26 16:16:00 +0100

    simple http server

--- 0x12-Node_JS_basic/4-http.js
+++ 0x12-Node_JS_basic/4-http.js
@@ -3,5 +3,5 @@ const http = require("http");
 const app = http.createServer((req, res) => {
   res.end("Hello Holberton School!");
 });
-app.listen("127.0.0.1:1245");
+app.listen(1245);
 module.exports = app;
