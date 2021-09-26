ff67d41a79705a329f52c28aa57fe7d3e022bbc1
karim <karim-bahri-18@outlook.fr>
2021-09-26 01:46:25 +0100

    readSync

--- 0x12-Node_JS_basic/2-read_file.js
+++ 0x12-Node_JS_basic/2-read_file.js
@@ -7,17 +7,20 @@ module.exports = (path) => {
     const nbFields = [0, 0];
     const csList = [];
     const SWEList = [];
-    console.log(`Number of students: ${dbs.length - 1}`);
+    let count = 0;
     for (element of dbs) {
       const fields = element.split(",");
       if (fields[3] === "SWE") {
         nbFields[0]++;
         SWEList.push(fields[0]);
+        count++;
       } else if (fields[3] === "CS") {
         nbFields[1]++;
         csList.push(fields[0]);
+        count++;
       }
     }
+    console.log(`Number of students: ${count}`);
     if (csList.length)
       console.log(
         `Number of students in CS: ${nbFields[1]}. List: ${csList.join(", ")}`
