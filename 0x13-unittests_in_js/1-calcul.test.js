8d42b25bbb4d656cbdd812f417df9bec1e25c761
karim <karim-bahri-18@outlook.fr>
2021-09-29 15:27:29 +0100

    sub/sub/div

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -5,11 +5,24 @@ describe("calculateNumber-test", function () {
 
   it("checking for simple operation", function () {
     assert.equal(calculateNumber("SUM", 1, 3), 4);
+    assert.equal(calculateNumber("SUM", 1.5, 3.5), 6);
+    assert.equal(calculateNumber("SUM", 1.4, 3.5), 5);
+    assert.equal(calculateNumber("SUM", 1.4, 3.4), 4);
 
     assert.equal(calculateNumber("SUBTRACT", 1.4, 4.5), -4);
+
+    assert.equal(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
+
+    assert.equal(calculateNumber("DIVIDE", 1.4, 0), "Error");
   });
   it("checking for edges cases", function () {
     assert.equal(isNaN(calculateNumber(1, 3)), true);
     assert.equal(isNaN(calculateNumber("DIVID", 1.4, 0)), true);
+    assert.equal(isNaN(calculateNumber()), true);
+    assert.equal(calculateNumber("SUM", -1.4, -3), -4);
+    assert.equal(calculateNumber("SUBTRACT", -1.4, -4.5), 3);
+    assert.equal(calculateNumber("DIVIDE", 1.4, -4.5), -0.25);
+    assert.equal(calculateNumber("DIVIDE", -1.4, 4.5), -0.2);
+    assert.equal(calculateNumber("DIVIDE", -1.4, -4.5), 0.25);
   });
 });
