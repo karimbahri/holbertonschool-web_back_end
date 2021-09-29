acffe6b29f234d156e072fd745b84810a7d47280
karim <karim-bahri-18@outlook.fr>
2021-09-29 17:07:28 +0100

    chai

--- 0x13-unittests_in_js/1-calcul.test.js
+++ 0x13-unittests_in_js/1-calcul.test.js
@@ -24,5 +24,6 @@ describe("calculateNumber-test", function () {
     assert.equal(calculateNumber("DIVIDE", 1.4, -4.5), -0.25);
     assert.equal(calculateNumber("DIVIDE", -1.4, 4.5), -0.2);
     assert.equal(calculateNumber("DIVIDE", -1.4, -4.5), 0.25);
+    assert.equal(calculateNumber("DIVIDE", 1.4, 0.4), "Error");
   });
 });
