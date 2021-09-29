3befb76dfa1295fe8053ca97085e963d36bf017f
karim <karim-bahri-18@outlook.fr>
2021-09-29 18:14:15 +0100

    chai

--- 0x13-unittests_in_js/2-calcul_chai.test.js
+++ 0x13-unittests_in_js/2-calcul_chai.test.js
@@ -1,28 +1,28 @@
 /* eslint-disable */
 describe("calculateNumber-test", function () {
-  const exprect = require("chai").expect;
-  const calculateNumber = require("./2-calcul_chai");
+  const assert = require("assert");
+  const calculateNumber = require("./1-calcul");
 
   it("checking for simple operation", function () {
-    exprect(calculateNumber("SUM", 1, 3)).to.equal(4);
-    exprect(calculateNumber("SUM", 1.5, 3.5)).to.equal(6);
-    exprect(calculateNumber("SUM", 1.4, 3.5)).to.equal(5);
-    exprect(calculateNumber("SUM", 1.4, 3.4)).to.equal(4);
+    assert.equal(calculateNumber("SUM", 1, 3), 4);
+    assert.equal(calculateNumber("SUM", 1.5, 3.5), 6);
+    assert.equal(calculateNumber("SUM", 1.4, 3.5), 5);
+    assert.equal(calculateNumber("SUM", 1.4, 3.4), 4);
 
-    exprect(calculateNumber("SUBTRACT", 1.4, 4.5)).to.equal(-4);
+    assert.equal(calculateNumber("SUBTRACT", 1.4, 4.5), -4);
 
-    exprect(calculateNumber("DIVIDE", 1.4, 4.5)).to.equal(0.2);
+    assert.equal(calculateNumber("DIVIDE", 1.4, 4.5), 0.2);
 
-    exprect(calculateNumber("DIVIDE", 1.4, 0)).to.equal("Error");
+    assert.equal(calculateNumber("DIVIDE", 1.4, 0), "Error");
   });
   it("checking for edges cases", function () {
-    exprect(isNaN(calculateNumber(1, 3))).to.equal(true);
-    exprect(isNaN(calculateNumber("DIVID", 1.4, 0))).to.equal(true);
-    exprect(isNaN(calculateNumber())).to.equal(true);
-    exprect(calculateNumber("SUM", -1.4, -3)).to.equal(-4);
-    exprect(calculateNumber("SUBTRACT", -1.4, -4.5)).to.equal(3);
-    exprect(calculateNumber("DIVIDE", 1.4, -4.5)).to.equal(-0.25);
-    exprect(calculateNumber("DIVIDE", -1.4, 4.5)).to.equal(-0.2);
-    exprect(calculateNumber("DIVIDE", -1.4, -4.5)).to.equal(0.25);
+    assert.equal(isNaN(calculateNumber(1, 3)), true);
+    assert.equal(isNaN(calculateNumber("DIVID", 1.4, 0)), true);
+    assert.equal(isNaN(calculateNumber()), true);
+    assert.equal(calculateNumber("SUM", -1.4, -3), -4);
+    assert.equal(calculateNumber("SUBTRACT", -1.4, -4.5), 3);
+    assert.equal(calculateNumber("DIVIDE", 1.4, -4.5), -0.25);
+    assert.equal(calculateNumber("DIVIDE", -1.4, 4.5), -0.2);
+    assert.equal(calculateNumber("DIVIDE", -1.4, -4.5), 0.25);
   });
 });
