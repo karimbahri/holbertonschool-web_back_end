486e30d0f308fe63dcb6421d21858b8f28c30827
karim <karim-bahri-18@outlook.fr>
2021-09-29 17:03:35 +0100

    chai

---
+++ 0x13-unittests_in_js/2-calcul_chai.js
@@ -0,0 +1,12 @@
+/* eslint-disable */
+module.exports = function (type, a, b) {
+  return type === "SUM"
+    ? Math.round(a) + Math.round(b)
+    : type === "SUBTRACT"
+    ? Math.round(a) - Math.round(b)
+    : type === "DIVIDE"
+    ? b !== 0
+      ? Math.round(a) / Math.round(b)
+      : "Error"
+    : "Error";
+};
