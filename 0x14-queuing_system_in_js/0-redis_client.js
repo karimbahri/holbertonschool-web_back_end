d80f456d45a7af917e12a5a83278a7bfcf70ec9b
karim <karim-bahri-18@outlook.fr>
2021-10-03 20:33:50 +0100

    create redis client

---
+++ 0x14-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,8 @@
+/* eslint disable */
+import redis from "redis";
+const client = redis.createClient();
+
+client.on("connect", () => console.log("Redis client connected to the server"));
+client.on("error", (err) =>
+  console.log(`Redis client not connected to the server: ${err}`)
+);
